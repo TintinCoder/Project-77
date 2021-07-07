65ea1814eab6e7410f8e36fca8d3414ef35b1790
TintinCoder <81453801+TintinCoder@users.noreply.github.com>
2021-07-07 16:46:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// First Part
+console.log("My First Console Project");
+// Second Part
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+// Third Part
+var a = 15;
+var b = 5;
+// Doing Addition
+console.log(a + b);
+// Doing Substraction
+console.log(a - b);
+// Doing Multiplication 
+console.log(a * b);
+// Doing Division 
+console.log(a / b);
+// Thanking 
+console.log("Thanks For This Project White Hat Junior");
